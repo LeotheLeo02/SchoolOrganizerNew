58b1219be87329dc63611ccc184b1027838803a6
Nathanael Suarez <nate.suarez@icloud.com>
2022-08-02 13:30:17 -0500

    Initial Commit

---
+++ SchoolOrganizer/SchoolOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolOrganizerApp.swift
+//  SchoolOrganizer
+//
+//  Created by Nate on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SchoolOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
