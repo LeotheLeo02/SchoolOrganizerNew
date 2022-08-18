5788a28318c62c9447fb1e7f739d835a0b54b8a7
Nathanael Suarez <nate.suarez@icloud.com>
2022-08-18 14:44:44 -0500

    Changes

--- SchoolOrganizer/TestViews/SessionEditView.swift
+++ SchoolOrganizer/TestViews/SessionEditView.swift
@@ -12,13 +12,71 @@ struct SessionEditView: View {
     @Environment(\.managedObjectContext) var managedObjContext
     @Environment(\.dismiss) var dismiss
     @State private var name = ""
+    @State private var newstart = Date()
+    @State private var newend = Date()
+    @State private var startday: Int = 0
+    @State private var changed = false
     var body: some View {
         VStack{
             if session.name != nil{
-            Text(session.name!)
-                .onAppear(){
-                    name = session.name!
-                }
+                VStack(alignment: .leading){
+                    HStack{
+                        Text(session.name!)
+                            .font(.system(size: 25, weight: .heavy, design: .rounded))
+                            .foregroundColor(changed ? .white : .gray)
+                            .animation(.easeInOut, value: changed)
+                        
+                            .onAppear(){
+                                name = session.name!
+                            }
+                        Spacer()
+                        Button {
+                            withAnimation{
+                            changed = true
+                                simpleSuccess()
+                                StudyDataController().editStudyTime(study: session, start: newstart, end: newend, context: managedObjContext)
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+                                    dismiss()
+                                }
+                            }
+                        } label: {
+                            if !changed{
+                            Text("Change")
+                            }
+                            if changed{
+                                Image(systemName: "checkmark.circle.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .symbolRenderingMode(.hierarchical)
+                                    .foregroundColor(.blue)
+                            }
+                        }
+                    }
+                    DatePicker(selection: $newstart) {
+                        Text("Current Starting Date: ")
+                            .font(.system(size: 20, weight: .bold, design: .rounded))
+                            .foregroundColor(changed ? .white :.gray)
+                            .animation(.easeInOut, value: changed)
+                    }.onAppear(){
+                        newstart = session.start!
+                    }
+                    DatePicker(selection: $newend, displayedComponents: .hourAndMinute) {
+                        Text("Current Ending Time: ")
+                            .font(.system(size: 20, weight: .bold, design: .rounded))
+                            .foregroundColor(changed ? .white :.gray)
+                            .animation(.easeInOut, value: changed)
+                    }.onAppear(){
+                        newend = session.end!
+                    }
+                    
+                }.padding()
+                    .background(changed ? .green : Color(.systemGray6))
+                    .animation(.easeInOut, value: changed)
+                    .cornerRadius(20)
+                    .padding()
+                    .shadow(color: changed ? .green :.gray, radius: 7)
+                    .animation(.easeInOut, value: changed)
             }
             Button(role: .destructive) {
                 UNUserNotificationCenter.current().getPendingNotificationRequests{ (notificationRequests) in
@@ -45,5 +103,9 @@ struct SessionEditView: View {
             }.buttonStyle(.bordered)
 
         }
+    }
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
     }
 }
